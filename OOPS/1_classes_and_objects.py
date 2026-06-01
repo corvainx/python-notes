eb1f1be2b0f39616d0bed6e41765f5e03bed266b
ishmweet <ishmeetsingharora07@gmail.com>
2026-06-01 19:30:32 +0530

    class variables

--- OOPS/1_classes_and_objects.py
+++ OOPS/1_classes_and_objects.py
@@ -10,10 +10,35 @@ class Car:
         self.year = year
         self.colour = colour
         self.for_sale = for_sale
+    
+    def drive(self):
+        print(f"You drive the {self.colour} {self.model}")
+
+    def stop(self):
+        print(f"You stop the {self.colour} {self.model}")
+
+    def describe(self):
+        print(f"{self.year} {self.colour} {self.model}")
 
 car1 = Car("Mustang", 2024, "red", False)
 car2 = Car("Corvette", 2025, "blue", True)
 car3 = Car("McLarren", 2026, "yellow", True)
+
+car1.describe()
+car2.describe()
+car3.describe()
+
+print()
+
+car1.drive()
+car2.drive()
+car3.drive()
+
+print()
+
+car1.stop()
+car2.stop()
+car3.stop()
 
 print(car1.model)
 print(car1.year)
