0ad369d770c8bbb0ad9fd30d9ddfd5e95d50f045
ishmweet <ishmeetsingharora07@gmail.com>
2026-06-01 19:59:07 +0530

    Inheritance :)

---
+++ OOPS/4_multiple_inheritance.py
@@ -0,0 +1,6 @@
+# multiple inheritance = inherit from more than one parent class
+#                        C(A, B)
+
+# multilevel inheritance = inherit from a parent which inherits from another parent
+#                          C(B) <- B(A) <- A
+
--- OOPS/4_multipule_inheritance.py
+++
@@ -1,6 +0,0 @@
-# multiple inheritance = inherit from more than one parent class
-#                        C(A, B)
-
-# multilevel inheritance = inherit from a parent which inherits from another parent
-#                          C(B) <- B(A) <- A
-
